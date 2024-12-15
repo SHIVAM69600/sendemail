ef903902fd8e6418b1b6b4b0e938c2fc1553485c
shivamm <thakreshivam48@gmail.com>
2024-12-15 15:11:44 +0530

    Initial commit

---
+++ src/main/java/com/shivam/controller/EmailController.java
@@ -0,0 +1,24 @@
+package com.shivam.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shivam.dto.EmailRequest.EmailRequest;
+import com.shivam.service.EmailService;
+
+@RestController
+@RequestMapping("/api")
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/send-email")
+    public String sendEmail(@RequestBody EmailRequest emailRequest) {
+        emailService.sendEmail(emailRequest.getTo(), emailRequest.getSubject(), emailRequest.getBody());
+        return "Email sent successfully!";
+    }
+}
